8f8073ff38baaa770cffe38303b56fdce7f41fba
Lcmkey <lcmkey@gmail.com>
2021-01-20 12:36:54 +0800

    update

--- bin/aws-vpc-cdk.ts
+++ bin/aws-vpc-cdk.ts
@@ -1,7 +1,34 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from '@aws-cdk/core';
-import { AwsVpcCdkStack } from '../lib/aws-vpc-cdk-stack';
+require("dotenv").config();
+
+import "source-map-support/register";
+import * as cdk from "@aws-cdk/core";
+
+import { AwsCdkVpcStack } from "../lib/aws-vpc-cdk-stack";
+
+/**
+ * Get variables from Env
+ */
+const {
+  PREFIX: prefix = "[STACK PREFIX NAME]",
+  STAGE: stage = "[DEPLOYMENT STAGE]",
+  CDK_ACCOUNT: accountId = "[AWS ACCOUNT ID]",
+  CDK_REGION: region = "ap-southeast-1",
+} = process.env;
+
+/**
+ * AWS defulat ENV config Definition
+ */
+const env = {
+  account: accountId,
+  region: region,
+};
 
 const app = new cdk.App();
-new AwsVpcCdkStack(app, 'AwsVpcCdkStack');
+new AwsCdkVpcStack(app, `${prefix}-${stage}-AwsCdkVpcStack`, {
+  env,
+  prefix,
+  stage,
+});
+
+app.synth();
--- lib/aws-vpc-cdk-stack.ts
+++ lib/aws-vpc-cdk-stack.ts
@@ -1,65 +1,120 @@
-import * as cdk from "@aws-cdk/core";
+import { Construct, Stack, StackProps } from "@aws-cdk/core";
 import { Vpc, SecurityGroup, SubnetType, Port, Peer } from "@aws-cdk/aws-ec2";
 
-export class AwsVpcCdkStack extends cdk.Stack {
-  vpc: Vpc;
-  ecsClusterSg: SecurityGroup;
-  elbSg: SecurityGroup;
-  efsSg: SecurityGroup;
-  efsDevEc2Sg: SecurityGroup;
+export interface AwsCdkVpcStackProps extends StackProps {
+  readonly prefix: string;
+  readonly stage: string;
+}
 
-  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+export class AwsCdkVpcStack extends Stack {
+  readonly vpc: Vpc;
+  readonly ecsClusterSg: SecurityGroup;
+  readonly elbSg: SecurityGroup;
+  readonly efsSg: SecurityGroup;
+  readonly efsDevEc2Sg: SecurityGroup;
+  readonly ingressSg: SecurityGroup;
+  readonly egressSg: SecurityGroup;
+
+  constructor(scope: Construct, id: string, props: AwsCdkVpcStackProps) {
     super(scope, id, props);
 
-    // define vpc
-    const vpc = new Vpc(this, "VPC", {
+    /**
+     * Get var from props
+     */
+    const { prefix, stage } = props;
+
+    /**
+     * Vpc Definition
+     * Additionally, if you want to launch an RDS instance into your VPC,
+     * make sure to set the property maxAzs to >=2 since RDS instances require at least two subnets each in a different AZ.
+     */
+    const vpc = new Vpc(this, `${prefix}-${stage}-Vpc`, {
       cidr: "172.17.0.0/16",
-      maxAzs: 3,
-      natGateways: 1,
+      maxAzs: 2,
+      natGateways: 0,
       subnetConfiguration: [
         {
           cidrMask: 24,
           name: "ingress",
-          subnetType: SubnetType.PUBLIC
-        }
-      ]
+          subnetType: SubnetType.PUBLIC,
+        },
+        {
+          cidrMask: 26,
+          name: "isolatedSubnet",
+          subnetType: SubnetType.ISOLATED,
+        },
+      ],
     });
 
-    // define security group
-    const efsSg = new SecurityGroup(this, "efsSg", {
-      securityGroupName: "efs-sg",
-      vpc
-    });
-    const ecsClusterSg = new SecurityGroup(this, "ecsClusterSg", {
-      securityGroupName: "ecs-cluster-sg",
-      vpc
-    });
-    const elbSg = new SecurityGroup(this, "elbSg", {
-      securityGroupName: "elb-sg",
-      vpc
-    });
-    const efsDevEc2Sg = new SecurityGroup(this, "efsDevEc2Sg", {
-      securityGroupName: "efs-dev-sg",
-      vpc
-    });
-    const rdsSg = new SecurityGroup(this, "rdsSg", {
-      securityGroupName: "rds-sg",
-      vpc
+    /**
+     * Security Group Definitions
+     */
+    const efsSg = new SecurityGroup(this, `${prefix}-${stage}-EFS-SG`, {
+      securityGroupName: `${prefix}-${stage}-EFS-SG`,
+      vpc,
     });
 
-    // add ingress rules
+    const ecsClusterSg = new SecurityGroup(
+      this,
+      `${prefix}-${stage}-ECS-Cluster-SG`,
+      {
+        securityGroupName: `${prefix}-${stage}-ECS-Cluster-SG`,
+        vpc,
+      },
+    );
+
+    const elbSg = new SecurityGroup(this, `${prefix}-${stage}-ECS-ELB-SG`, {
+      securityGroupName: `${prefix}-${stage}-ECS-ELB-SG`,
+      vpc,
+    });
+
+    const efsDevEc2Sg = new SecurityGroup(
+      this,
+      `${prefix}-${stage}-EFS-Dev-EC2-SG`,
+      {
+        securityGroupName: `${prefix}-${stage}-EFS-Dev-EC2-SG`,
+        vpc,
+      },
+    );
+
+    const rdsSg = new SecurityGroup(this, `${prefix}-${stage}-RDS-SG`, {
+      securityGroupName: `${prefix}-${stage}-RDS-SG`,
+      vpc,
+    });
+
+    const ingressSg = new SecurityGroup(this, `${prefix}-${stage}-Ingress-SG`, {
+      vpc,
+      allowAllOutbound: false,
+      securityGroupName: `${prefix}-${stage}-Ingress-SG`,
+    });
+
+    const egressSg = new SecurityGroup(this, `${prefix}-${stage}-Egress-SG`, {
+      vpc,
+      allowAllOutbound: false,
+      securityGroupName: `${prefix}-${stage}-Egress-SG`,
+    });
+
+    /**
+     * Add ingress rules
+     */
     elbSg.addIngressRule(Peer.anyIpv4(), Port.tcp(80));
     ecsClusterSg.addIngressRule(elbSg, Port.allTcp());
     ecsClusterSg.addIngressRule(Peer.anyIpv4(), Port.tcp(22));
     efsSg.addIngressRule(ecsClusterSg, Port.tcp(2049));
     efsSg.addIngressRule(efsDevEc2Sg, Port.tcp(2049));
     rdsSg.addIngressRule(ecsClusterSg, Port.tcp(3306));
+    ingressSg.addIngressRule(Peer.ipv4("172.17.0.0/16"), Port.tcp(3306));
+    egressSg.addEgressRule(Peer.anyIpv4(), Port.tcp(80));
 
-    // assign value to variable
+    /**
+     * Assign value to variable
+     */
     this.vpc = vpc;
     this.ecsClusterSg = ecsClusterSg;
     this.elbSg = elbSg;
     this.efsSg = efsSg;
     this.efsDevEc2Sg = efsDevEc2Sg;
+    this.ingressSg = ingressSg;
+    this.egressSg = egressSg;
   }
 }
